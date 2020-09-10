1321d57ca7b4f9962b80cae6fae74260699746d0
Evghenii Kunitski <kunitsky@gmail.com>
2020-09-10 22:40:54 +0200

    переделал if, но main пока не осилил

--- homework1/diff_string.py
+++ homework1/diff_string.py
@@ -11,23 +11,45 @@
 
 
 def string_compare(str1, str2):
-    if type(str1) == type(str2) == str:
-        print("string detected")
-        if str1 == str2:
-            return 1
-        elif len(str1) > len(str2):
-            return 2
-        elif str2 == "learn":
-            return 3
-    else:
-        # если немного преписать первое условие - можно обойтись без else и сразу возвращать 0
+    if type(str1) != str or type(str2) != str:
         return 0
+    if str1 == str2:
+        return 1
+    elif len(str1) > len(str2):
+        return 2
+    elif str2 == "learn":
+        return 3
 
+    # если немного преписать первое условие - можно обойтись без else и сразу возвращать 0
+    # переделал
 
 # по заданию не очень корректно сказано, здесь лучше самому написать код который
 # будет вызывать функцию с разными параметрами
 # ps и это все лучше обернуть тоже в функцию main
-input_str1 = input("введите первую строку: ")
-input_str2 = input("введите вторую строку: ")
+#main я сегодня не потяну, но несколько вводов сделаю
+#input_str1 = input("введите первую строку: ")
+input_str1 = 'blablacar'
+#input_str2 = input("введите вторую строку: ")
+input_str2 = 'ajo'
+print(input_str1,input_str2)
+print(string_compare(input_str1, input_str2))
 
+
+input_str1 = 'work'
+#input_str2 = input("введите вторую строку: ")
+input_str2 = 'learn'
+print(input_str1,input_str2)
+print(string_compare(input_str1, input_str2))
+
+input_str1 = 'learn1'
+#input_str2 = input("введите вторую строку: ")
+input_str2 = 'learn'
+print(input_str1,input_str2)
+print(string_compare(input_str1, input_str2))
+
+
+input_str1 = 'student'
+#input_str2 = input("введите вторую строку: ")
+input_str2 = 1
+print(input_str1,input_str2)
 print(string_compare(input_str1, input_str2))
